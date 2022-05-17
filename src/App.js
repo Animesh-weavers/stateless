4e26a030fda0b1ac597209d8dde239c76ef70055
Animesh-weavers <animesh.dey@weavers-web.com>
2022-05-17 13:58:22 +0530

    using lifecycle method & state and props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import Users from './components/Users';
+import React, { Component } from "react";
+import UserFinder from "./components/UserFinder";
 
-function App() {
-  return (
-    <div>
-      <Users />
-    </div>
-  );
+export default class App extends Component {
+  // componentDidMount(){
+  //   console.log("mount");
+  // }
+  // componentDidUpdate(){
+  //   console.log("update");
+  // }
+  // componentWillUnmount(){
+  //   console.log("unmount")
+  // }
+  render() {
+    return (
+      <>
+        <UserFinder />
+      </>
+    );
+  }
 }
-
-export default App;
--- src/components/User.js
+++ src/components/User.js
@@ -1,7 +1,17 @@
-import classes from './User.module.css';
+import classes from "./User.module.css";
+import { Component } from "react";
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
-};
+class User extends Component {
+  componentWillUnmount() {
+    console.log("unmount");
+  }
+  render() {
+    return <li className={classes.user}>{this.props.name}</li>;
+  }
+}
+
+// const User = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
 
 export default User;
